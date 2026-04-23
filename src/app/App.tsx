6fb0a21577d0c0e5d3fba64e61e0a2a18cb52328
Oldemar Gonzalez <technest.pa@gmail.com>
2026-04-23 13:10:43 -0500

    fix: remove duplicate AnimatePresence blocking first render

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { AnimatePresence } from "framer-motion";
 
 export default function App() {
-  return (
-    <AnimatePresence mode="wait">
-      <RouterProvider router={router} />
-    </AnimatePresence>
-  );
+  return <RouterProvider router={router} />;
 }
